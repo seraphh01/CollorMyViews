5107426cd5a9e733649e970a70cbda9f2457a2c1
seraphh01 <71792603+seraphh01@users.noreply.github.com>
2022-02-13 16:34:36 +0200

    Initial commit

---
+++ app/src/main/java/com/seraphicgames/collormyviews/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.seraphicgames.collormyviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
